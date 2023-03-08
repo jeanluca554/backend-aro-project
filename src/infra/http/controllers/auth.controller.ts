353cf9157a058f20d2823758bf273acad872ecfa
Jean Luca <jean@jean-PC>
2023-03-08 13:11:39 -0300

    style(auth.controller): remove imports that are not used

--- src/infra/http/controllers/auth.controller.ts
+++ src/infra/http/controllers/auth.controller.ts
@@ -1,20 +1,15 @@
 import {
-  Body,
   Controller,
-  Get,
   HttpCode,
   HttpStatus,
-  Param,
   Post,
   Request,
   UseGuards,
 } from '@nestjs/common';
-import { CreateUser } from '@app/use-cases/user/create-user';
-import { CreateUserBody } from '../dtos/create-user-body';
-import { UserViewModel } from '../view-models/user-view-model';
-import { LocalAuthGuard } from '@infra/guards/local-auth.guard';
-import { AuthService } from '@app/use-cases/auth/auth.service';
+
 import { AuthRequest } from '@app/models/authRequest';
+import { AuthService } from '@app/use-cases/auth/auth.service';
+import { LocalAuthGuard } from '@infra/guards/local-auth.guard';
 
 @Controller()
 export class AuthController {
