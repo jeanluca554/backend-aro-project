203bc4f02b770d141b7f4a3e94fb84af53fec201
Jean Luca <jean@jean-PC>
2023-03-09 09:15:29 -0300

    fix(auth): login endpoint should be public

--- src/infra/http/controllers/auth.controller.ts
+++ src/infra/http/controllers/auth.controller.ts
@@ -10,11 +10,13 @@ import {
 import { AuthRequest } from '@app/models/authRequest';
 import { AuthService } from '@app/use-cases/auth/auth.service';
 import { LocalAuthGuard } from '@infra/guards/local-auth.guard';
+import { IsPublic } from '@infra/decorators/is-public.decorator';
 
 @Controller()
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @IsPublic()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
