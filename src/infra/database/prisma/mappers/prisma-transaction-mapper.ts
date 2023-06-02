b0c08a7768980833b0d7cd4d0d9765c57d0c53bc
Jean Luca <jean@jean-PC>
2023-06-02 19:33:04 -0300

    style(transaction): remove unused code

--- src/infra/database/prisma/mappers/prisma-transaction-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-transaction-mapper.ts
@@ -1,23 +1,5 @@
 // import { Notification as RawNotification } from '@prisma/client';
 import { Transaction } from '@app/entities/transaction/transaction';
-import { ProductsOnTransactions } from '@prisma/client';
-
-type TransactionType = {
-  customer: string;
-  category: string;
-  identity: string;
-  amountPaid: number;
-  paymentMethod: string;
-  installments: number;
-  product: string;
-  paymentDate: string;
-};
-
-type ProductsOnTransactionsType = {
-  description: string | undefined;
-  paymentMethod: string;
-  installments: number;
-};
 
 export class PrismaTransactionMapper {
   static toPrisma(transaction: Transaction) {
@@ -36,28 +18,6 @@ export class PrismaTransactionMapper {
       description: transaction.description,
       tid: transaction.tid,
       authorizationCode: transaction.authorizationCode,
-    };
-  }
-
-  // static toDomain(raw: RawNotification): Notification {
-  //   return new Notification(
-  //     {
-  //       category: raw.category,
-  //       content: new Content(raw.content),
-  //       recipientId: raw.recipientId,
-  //       readAt: raw.readAt,
-  //       canceledAt: raw.canceledAt,
-  //       createdAt: raw.createdAt,
-  //     },
-  //     raw.id,
-  //   );
-  // }
-
-  static toDomain(raw: ProductsOnTransactionsType): ProductsOnTransactionsType {
-    return {
-      description: raw.description,
-      installments: raw.installments,
-      paymentMethod: raw.paymentMethod,
     };
   }
 }
